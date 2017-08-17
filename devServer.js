b47ea10213d7a5446449b2f5be113e22e7cdf067
Robin <almigod0718@gmail.com>
2017-08-17 19:28:03 +0800

    Init express dev server

---
+++ devServer.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack')
+const express = require('express')
+const request = require('request')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+const bodyParser = require('body-parser')
+const fs = require('fs')
+const PORT = process.env.PORT || 8080
+const config = require('./builderconfig/webpack.config.local.js')
+const compiler = webpack(config)
+const server = express()
+const router = express.Router()
+
+router.post('/mock/*', (req, res) => {
+  const requestQueryParams = req.path
+        .split('/')
+        .slice(1)
+        .join('/')
+
+  res.send(JSON.parse(fs.readFileSync(requestQueryParams.replace(/\?.*$/, ''), 'utf8')))
+})
+
+router.get('/mock/*', (req, res) => {
+  const requestQueryParams = req.path
+        .split('/')
+        .slice(1)
+        .join('/')
+
+  res.send(JSON.parse(fs.readFileSync(requestQueryParams.replace(/\?.*$/, ''), 'utf8')))
+})
+
+router.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname })
+})
+
+server.use(webpackDevMiddleware(compiler, {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  headers: { 'Access-Control-Allow-Origin': '*' },
+}))
+server.use(webpackHotMiddleware(compiler))
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+server.use('/', express.static('mock'))
+server.use(router)
+
+server.listen(PORT, (err) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.info('using congfig.local.js'.green)
+    console.info(`==> Listening on port ${PORT}. Open up http://localhost:${PORT}/ in your browser.`, PORT, PORT)
+  }
+})
